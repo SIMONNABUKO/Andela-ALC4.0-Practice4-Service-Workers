17bd5765be038d811c45e7e33e8bf64a1765dd12
Simon Angatia <simonnabuko@gmail.com>
2019-07-04 21:38:41 -0700

    update service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,7 @@
+"use strict";
+self.addEventListener('install', function(e){
+    console.log('Install event:', e);
+});
+self.addEventListener('activate', function(e){
+    console.log('Activate event:', e);
+});
